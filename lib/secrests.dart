7906c8f864ab4ba3e865fd2f9766a163acb8de16
clandestine-612k <bandana.shaw612@gmail.com>
2024-03-24 17:29:05 +0530

    I have created a Flutter project

---
+++ lib/secrests.dart
@@ -0,0 +1,1 @@
+const openWeatherAPIkey = '843ce7319c2240eae6420dbe45efe6de';
---
+++ lib/weather_screen.dart
@@ -0,0 +1,209 @@
+import 'dart:convert';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'Additional_info.dart';
+import 'hourly_weather_forecast.dart';
+import 'package:http/http.dart' as http;
+
+import 'secrests.dart';
+
+class WeatherScreen extends StatefulWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  late Future<Map<String, dynamic>> weather;
+  Future<Map<String, dynamic>> getCurrentWeather() async {
+    try {
+      String cityName = 'Kolkata';
+      final res = await http.get(
+        Uri.parse(
+          'https://api.openweathermap.org/data/2.5/forecast?q=$cityName&APPID=$openWeatherAPIkey',
+        ),
+      );
+      final data = jsonDecode(res.body);
+
+      if (data['cod'] != '200') {
+        throw 'An unexpected error occured';
+      }
+      return data;
+      //(data['list'][0]['main']['temp']);
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    weather = getCurrentWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Weather App',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  weather = getCurrentWeather();
+                });
+              },
+              icon: const Icon(Icons.refresh))
+        ],
+      ),
+      body: FutureBuilder(
+        future: weather,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final data = snapshot.data!;
+          final currentWeatherData = data['list'][0];
+          final currenttemp = currentWeatherData['main']['temp'];
+          final currentsky = currentWeatherData['weather'][0]['main'];
+          final currentPressure =
+              currentWeatherData['main']['pressure'].toString();
+          final currentWindSpeed =
+              currentWeatherData['wind']['speed'].toString();
+          final currentHumidity =
+              currentWeatherData['main']['humidity'].toString();
+
+          if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          }
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: Card(
+                    elevation: 16,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              Text(
+                                '$currenttemp K',
+                                style: const TextStyle(
+                                    fontSize: 32, fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              Icon(
+                                currentsky == 'Clouds' || currentsky == 'Rain'
+                                    ? Icons.cloud
+                                    : Icons.sunny,
+                                size: 64,
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              Text(
+                                currentsky,
+                                style: const TextStyle(fontSize: 20),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              const Text(
+                                'Kolkata',
+                                style: TextStyle(
+                                    fontSize: 24, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Hourly Forecast',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 120,
+                  child: ListView.builder(
+                    itemCount: 5,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      final hourlyFORECAST = data['list'][index + 1];
+                      final hourlySky =
+                          data['list'][index + 1]['weather'][0]['main'];
+                      final time = DateTime.parse(hourlyFORECAST['dt_txt']);
+                      return HourlyForecast(
+                          time: DateFormat.Hm().format(time),
+                          icon: hourlySky == 'Clouds' || hourlySky == 'Rain'
+                              ? Icons.cloud
+                              : Icons.sunny,
+                          temp: hourlyFORECAST['main']['temp'].toString());
+                    },
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 16,
+                ),
+                const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Additional Information',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    )),
+
+                //additional information cards
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Addwidget(
+                      icon: Icons.water_drop,
+                      label: 'Humidity',
+                      value: currentHumidity,
+                    ),
+                    Addwidget(
+                      icon: Icons.air,
+                      label: 'Wind Speed',
+                      value: currentWindSpeed,
+                    ),
+                    Addwidget(
+                      icon: Icons.beach_access,
+                      label: 'Pressure',
+                      value: currentPressure,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
